7eaa7167af282868e3851954ed0b3f8ee1ace6e5
Nathan <viniciosnathan@gmail.com>
2025-11-25 16:51:15 -0300

    refactor: simplify index.ts by exporting runGenerateGraph directly from graph module

--- packages/agent-graph/src/index.ts
+++ packages/agent-graph/src/index.ts
@@ -1,18 +1,1 @@
-import type { GenerateRequest, GenerateResult } from "core-runtime";
-import { createGenerateGraph } from "./graph";
-
-const app = createGenerateGraph();
-
-export async function runGenerateGraph(
-  request: GenerateRequest,
-): Promise<GenerateResult> {
-  const finalState = (await (app as any).invoke({
-    request,
-  })) as { result?: GenerateResult };
-
-  if (!finalState.result) {
-    throw new Error("Graph finished without a result");
-  }
-
-  return finalState.result;
-}
+export { runGenerateGraph } from "./graph";
