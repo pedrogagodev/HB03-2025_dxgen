2f57c25cd6f203390add70b3dcafa7ae45c13884
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-11-25 22:17:03 -0300

    feat: export core functionalities from rag package including file chunking, embeddings, and Pinecone synchronization

---
+++ packages/rag/src/index.ts
@@ -0,0 +1,12 @@
+export { chunkProjectFiles } from "./chunker.js";
+export { createEmbeddings } from "./embeddings.js";
+export { scanProjectFiles } from "./file-scanner.js";
+export {
+  buildIndex,
+  resetPineconeNamespace,
+  syncChunksToPinecone
+} from "./pinecone-sync.js";
+export { runRagPipeline } from "./pipeline.js";
+export { createRetriever } from "./retriever.js";
+export * from "./types.js";
+
