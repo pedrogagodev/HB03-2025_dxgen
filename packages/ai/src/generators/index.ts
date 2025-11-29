c843d623f4519ae50838c8dbad76098e4dd123e4
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-11-29 10:18:18 -0300

    feat: add centralized generator exports for README, API docs, diagrams, and summaries

---
+++ packages/ai/src/generators/index.ts
@@ -0,0 +1,5 @@
+export { generateReadme } from "./readme.generator";
+export { generateApiDocs } from "./api-docs.generator";
+export { generateDiagrams } from "./diagram.generator";
+export { generateSummary } from "./summary.generator";
+
